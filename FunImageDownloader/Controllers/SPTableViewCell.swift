a4f50e90634997093c94b2b60772babddaf7bdc0
Stanislav Popovych <popovychs@gmail.com>
2016-04-14 19:32:51 +0300

    add ui elements

---
+++ FunImageDownloader/Controllers/SPTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SPTableViewCell.swift
+//  FunImageDownloader
+//
+//  Created by popovychs on 14.04.16.
+//  Copyright © 2016 popovychs. All rights reserved.
+//
+
+import UIKit
+
+class SPTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
